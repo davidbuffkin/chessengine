61795aab920accbd150eb325c391d8ccea7b5810
David Buffkin <david@Davids-MacBook-Pro.local>
2020-08-23 03:34:44 -0400

    Simple graphics

--- engine.py
+++ engine.py
@@ -2,30 +2,60 @@
 
 import chess
 import math
-from random import random
+from random import random, shuffle
+
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str) ...
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 
 values = {'P' : 100, 'R' : 500, 'N' : 300, 'B' : 300, 'Q' : 900, 'K' : 0, 'k' : 0,\
           'p' : -100, 'r' : -500, 'n' : -300, 'b' : -300, 'q' : -900, "\n" : 0, "." : 0, " " : 0}
 
+
 def evaluate(board, black):
-    if board.is_game_over():
-        return math.inf if (board.turn == chess.WHITE) == black else -math.inf
+    if board.is_checkmate():
+        return 1000000 if (board.turn == chess.WHITE) == black else -1000000
     return (-1 if black else 1) * sum([values[c] for c in str(board)])
 
 
-def search(board : chess.Board, black, depth, alpha, beta):
+def search(board : chess.Board, black, depth, alpha, beta, top):
 
     best = [-math.inf, None]
 
-    for move in board.legal_moves:
+    moves = list(board.legal_moves)
+    shuffle(moves) #TODO remove this when evaluate is better and for move ordering
+    
+
+    for i, move in enumerate(moves):
+        if top:
+            printProgressBar(i, len(moves), "Thinking...")
 
         board.push(move)
 
         moveScore = None
-        if depth <= 0:
+        if board.is_stalemate():
+            board.pop()
+            moveScore =  -1000000 if evaluate(board, black) > 0 == black else 0
+            board.push(move)
+        elif depth <= 0:
             moveScore = evaluate(board, black)
         else:
-            moveScore = -search(board, not black, depth - 1, -beta, -alpha)[0]
+            moveScore = -search(board, not black, depth - 1, -beta, -alpha, False)[0]
         
         board.pop()
 
@@ -34,10 +64,12 @@ def search(board : chess.Board, black, depth, alpha, beta):
         if moveScore > best[0]: best = [moveScore, move]
         if best[0] > alpha: alpha = best[0]
         if alpha >= beta: return [alpha, None]
-
+    if top:
+        printProgressBar(1, 1, "Done Thinking!")
+        #print("Returning " + str(best))
     return best
 
 def getMove(board, difficulty):
-    return search(board, board.turn == chess.BLACK, difficulty, -math.inf, math.inf)[1]
+    return search(board, board.turn == chess.BLACK, difficulty, -math.inf, math.inf, True)[1]
 
 
--- game.py
+++ game.py
@@ -1,15 +1,18 @@
 #David Buffkin
 
-#This is just a CLI to play with the engine.
+#This is inital graphics testing
 
 import argparse
 import chess
 from random import choice
 from engine import getMove
+import chess.svg
+import webview
 
 parser = argparse.ArgumentParser(description='Play a game of chess.')
 parser.add_argument('-d', '--difficulty', help='Engine difficulty (0 for random)', type=int, default=0)
-parser.add_argument('-b', '--black', help='Play as black', action='store_true')
+#parser.add_argument('-b', '--black', help='Play as black', action='store_true')
+parser.add_argument('-g', '--graphics', help='Display a graphic of the game', action='store_true')
 args = parser.parse_args()
 
 board = chess.Board()
@@ -25,30 +28,51 @@ def getPlayerMove():
         print(f"{moveString} is not a valid move. Try again.\n")
         return getPlayerMove()
 
+def getRandomMove():
+    return choice(list(board.legal_moves))
+
 def getEngineMove():
     if args.difficulty == 0:
-        return choice(list(board.legal_moves))
-    return getMove(board, args.difficulty)
+        return getRandomMove()
+    move = getMove(board, args.difficulty)
+    if not move:
+        return getRandomMove()
+    return move
 
-print("\n" +  str(board) +  "\n")
-while not board.is_game_over():
-    move = getPlayerMove()
-    board.push(move)
-    print("\n" +  str(board) + "\n")
-    if not board.is_game_over():
-        move = getEngineMove()
-        print("Engine moves " + move.uci())
-        board.push(move)
-        print("\n" + str(board) + "\n")
-
-if board.is_stalemate():
-    print("Stalemate.")
-else:
-    if board.is_checkmate():
-        print("Checkmate!")
-    if board.turn == chess.WHITE:
-        print("White wins.")
+def display_board(window):
+    if args.graphics:
+        window.load_html(chess.svg.board(board))
     else:
-        print("Black wins.")
+        print("\n" +  str(board) +  "\n")
 
 
+def game_loop(window):
+    if args.graphics:
+        window.resize(420, 420)
+    display_board(window)
+    while not board.is_game_over():
+        move = getPlayerMove()
+        board.push(move)
+        display_board(window)
+        move = getEngineMove()
+        if not move: break
+        board.push(move)
+        display_board(window)
+        
+    if board.is_stalemate():
+        print("Stalemate.")
+    else:
+        if board.is_checkmate():
+            print("Checkmate!")
+        if board.turn == chess.BLACK:
+            print("White wins.")
+        else:
+            print("Black wins.")
+
+
+if args.graphics:
+    window = webview.create_window('Chess')
+    window.on_top = True
+    webview.start(game_loop, window)
+else:
+    game_loop(None)
